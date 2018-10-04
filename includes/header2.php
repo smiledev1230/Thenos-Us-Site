a7788751f62f518f413775335ee8eac6f1556c09
Full-Static DEV <smilemonkey1230@outlook.com>
2018-10-04 23:29:53 +0700

    fix home2.php submenu issue

--- includes/header2.php
+++ includes/header2.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
-<html><head>
+<html>
+    <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-       <title><?php echo $title; ?> Thenos | PlayBox - In-Wall & In-Ceiling Solution for Sonos  </title>
-        
+        <title><?php echo $title; ?> Thenos | PlayBox - In-Wall & In-Ceiling Solution for Sonos  </title>
         <meta name="description" content="Thenos is based in Dallas, TX and the makers of the PlayBox. The PlayBox by thenos is an in-wall 
 enclosure made for the Sonos Play:1">
         <meta name="keywords" content="">
-        
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
         <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet" type="text/css">
         <link href="http://fonts.googleapis.com/css?family=Raleway:500,400,300" rel="stylesheet" type="text/css">
@@ -28,72 +26,57 @@ enclosure made for the Sonos Play:1">
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/font-awesome.min.css">
         <link rel="stylesheet" href="css/style.css">
-        <!-- Color CSS -->
         <link rel="stylesheet" href="css/colors/blue.css">
-
-        <!-- DARK CSS-->
-        <!--        <link rel="stylesheet" href="css/dark.css">-->
-
         <link rel="stylesheet" href="css/responsive.css">
+        <link rel="stylesheet" href="catalog/zenchat/cosmetic_zenchat.css"/>
+        <link rel="stylesheet" href="css/sm-core-css.css">
+        <link rel="stylesheet" href="css/sm-simple.css">
+
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
-        
-<script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
+        <script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="catalog/zenchat/cookie.js"></script>
+        <script type="text/javascript" src="js/jquery.smartmenus.js"></script>
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
 
-<link rel="stylesheet" style="text/css" href="catalog/zenchat/cosmetic_zenchat.css"/>
-<script type="text/javascript" src="catalog/zenchat/cookie.js"></script>
-
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="theme-color" content="#ffffff">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-<meta name="theme-color" content="#ffffff">
-<style>
-
-@font-face {
-    font-family: "GothamMedium";
-    src: url("/fonts/GothamMedium_1.ttf")  format("truetype");
-}
-
-.header-text {
-    font-size: 14px;
-    font-family:"GothamMedium";
-    color: #3aa5f2;
-    line-height: 34px;
-}
-.header-text:hover {
-    color: #EA9107;
-}
-</style>
+        <style>
+            @font-face {
+                font-family: "GothamMedium";
+                src: url("/fonts/GothamMedium_1.ttf")  format("truetype");
+            }
+            .header-text {
+                font-size: 14px;
+                font-family:"GothamMedium";
+                color: #3aa5f2;
+                line-height: 34px;
+            }
+            .header-text:hover {
+                color: #EA9107;
+            }
+        </style>
     </head>
     <body data-spy="scroll" data-target="#main-navbar">
-    
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-
         <div class="loader" style="display: none;">
             <div class="loaded" style="display: none;">&nbsp;</div>
         </div>
-
-
         <div id="page" class="page">
-
-
-
-            
             <!-- MENU Section-->
             <header>
                 <nav class="navbar fixed-nav navbar-default  navbar-fixed-top" id="main-navbar" role="navigation" style="min-height:0px;">
                 </nav>
             </header>
-
             <!-- MENU Section-->
-            
             <header>
                 <nav class="navbar fixed-nav navbar-default  navbar-fixed-top" id="main-navbar" role="navigation">
                     <div class="header-title">
@@ -113,36 +96,38 @@ enclosure made for the Sonos Play:1">
                         </div>  <!--end navbar-header -->
 
                         <div class="collapse navbar-collapse home-header" id="navbar-collapse">
-
                             <ul class="nav navbar-nav">
-                                
-                                
                                 <li><a href=<?php if(!$home) echo 'home.php'; ?>#models>models</a></li>
                                 <li><a href="/commercial.php">commercial</a></li>
                                 <!--<li><a href="http://www.thenos.us/Thenos%20PlayBox%20Spec%20Sheet%202016.pdf" target="_blank">PDF</a></li>-->
-                                 <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Resources</a>
-          <ul class="dropdown-menu">
-            <li><a href="http://www.thenos.us/Thenos%20PlayBox%20Spec%20Sheet%202016.pdf" target="_blank">PlayBox Spec Sheet</a></li>
-            <li><a href="#" target="_blank">PB1-W | In-Wall PlayBox One Sheet</a></li>
-            <li><a href="#" target="_blank">PB1-C | In-Ceiling PlayBox One Sheet</a></li>
-            <li><a href="#" target="_blank">PlayBox Commercial Overview</a></li>
-            <li><a href="#" target="_blank">Installation Instructions</a></li>
-            <li><a href="#" target="_blank">Cutting Templates</a></li>
-            <li><a href="#" target="_blank">PB1-C Resources</a></li>
-            <li><a href="http://www.thenos.us/pdf/Thenos%20Streaming%20Sound%20System%205x7%20PDF%20Brochure.pdf" target="_blank">PlayBox Overview</a></li>
-            <li><a href="http://www.thenos.us/pdf/Thenos PlayBox PB1 - PDF Wall Template.pdf" target="_blank">Install Template - PB1-W</a></li>
-           
-          </ul>
-        </li>
+                                <li class="dropdown">
+                                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Resources</a>
+                                  <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#" target="_blank">PB1-W | In-Wall PlayBox Spec Sheet</a></li>
+                                    <li><a href="#" target="_blank">PB1-C | In-Ceiling PlayBox Spec Sheet</a></li>
+                                    <li><a href="#" target="_blank">PlayBox Commercial Overview</a></li>
+                                    <li class="dropdown-submenu">
+                                        <a tabindex="-1" href="#">Installation Instructions</a>
+                                        <ul class="dropdown-menu">
+                                            <li><a tabindex="-1" href="#" target="_blank">PB1-W | In-Wall PlayBox</a></li>
+                                            <li><a href="#" target="_blank">PB1-C | In-Ceiling PlayBox</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown-submenu">
+                                        <a tabindex="-1" href="#">Cutting Templates</a>
+                                        <ul class="dropdown-menu">
+                                            <li><a tabindex="-1" href="#" target="_blank">PB1-W | Wall Template</a></li>
+                                            <li><a href="#" target="_blank">PB1-C | Ceiling Template</a></li>
+                                        </ul>
+                                    </li>
+                                  </ul>
+                                </li>
                                 <li><a href=<?php if(!$home) echo 'home.php'; ?>#screenshot>videos/photos</a></li>
                                 <li class="activemenu"><a href="dealer.php">resellers</a></li>
                                 <!--<li><a href="about.php">about</a></li>-->
                                 <li><a href="contact.php">contact</a></li>
-                                <li class="home-shop-btn">
-                                        <a href="http://www.thenos.us/index.php?route=product/category&path=59" class="btn btn-default download-btnsmall" id="downloadsmall">shop</a></li>
-                                <li class="home-sign-btn">
-                                        <a href="http://www.thenos.us/index.php?route=account/login" class="btn btn-default download-btnsmall" id="downloadsmallwhite">sign-in</a></li>
+                                <li class="home-shop-btn"><a href="http://www.thenos.us/index.php?route=product/category&path=59" class="btn btn-default download-btnsmall" id="downloadsmall">shop</a></li>
+                                <li class="home-sign-btn"><a href="http://www.thenos.us/index.php?route=account/login" class="btn btn-default download-btnsmall" id="downloadsmallwhite">sign-in</a></li>
                             </ul>
                         </div>  <!--end collapse -->
                     </div>  <!--end container -->
